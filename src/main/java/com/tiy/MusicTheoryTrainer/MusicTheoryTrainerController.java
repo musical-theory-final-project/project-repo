4d6d7d509f168e33af3b75a43a9fb97b5a642cb3
Clayton Strickland <cjstrickland5@gmail.com>
2016-10-10 13:05:40 -0400

    Initial commit: Working on making a view with a staff

---
+++ src/main/java/com/tiy/MusicTheoryTrainer/MusicTheoryTrainerController.java
@@ -0,0 +1,10 @@
+package com.tiy.MusicTheoryTrainer;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by RdDvls on 10/10/16.
+ */
+@Controller
+public class MusicTheoryTrainerController {
+}
