d1fe1b360d5e1bcdcc28f03e42871c34ff6a878a
Richard Blanch <bricerb@gmail.com>
2016-10-10 15:16:19 -0400

    Score is showing

--- src/main/resources/static/js/score.js
+++ src/main/resources/static/js/score.js
@@ -1,36 +1,14 @@
-//const VF = Vex.Flow;
-//
-//// Create an SVG renderer and attach it to the DIV element named "boo".
-//var vf = new VF.Factory({renderer: {selector: 'boo'}});
-//var score = vf.EasyScore();
-//var system = vf.System();
-//
-//system.addStave({
-//  voices: [score.voice(score.notes('C#5/q, B4, A4, G#4'))]
-//}).addClef('treble').addTimeSignature('4/4');
-//
-//vf.draw();
-
-
-
-VF = Vex.Flow;
+const VF = Vex.Flow;
 
 // Create an SVG renderer and attach it to the DIV element named "boo".
-var div = document.getElementById("boo");
-console.log("div:" + div);
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-console.log("renderer: " + renderer);
+var vf = new VF.Factory({renderer: {selector: 'boo'}});
+var score = vf.EasyScore();
+var system = vf.System();
 
-// Configure the rendering context.
-renderer.resize(500, 500);
-var context = renderer.getContext();
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+system.addStave({
+  voices: [score.voice(score.notes('C#5/q, B4, A4, G#4'))]
+}).addClef('treble').addTimeSignature('4/4')
+//voices: [score.voice(score.notes('B4'))]
+//}).addClef('treble');
 
-// Create a stave of width 400 at position 10, 40 on the canvas.
-var stave = new VF.Stave(10, 40, 400);
-
-// Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("4/4");
-
-
-console.log("score.js is working");
+vf.draw();
