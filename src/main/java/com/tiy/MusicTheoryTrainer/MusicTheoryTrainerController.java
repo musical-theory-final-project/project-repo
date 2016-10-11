19c4ac619676b55fa36a9952604ef0efb80bfd1d
Clayton Strickland <cjstrickland5@gmail.com>
2016-10-11 12:51:30 -0400

    made seperate html pages for login and register; added controller endpoint for register

--- src/main/java/com/tiy/MusicTheoryTrainer/MusicTheoryTrainerController.java
+++ src/main/java/com/tiy/MusicTheoryTrainer/MusicTheoryTrainerController.java
@@ -1,5 +1,6 @@
 package com.tiy.MusicTheoryTrainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,8 @@ import javax.servlet.http.HttpSession;
  */
 @Controller
 public class MusicTheoryTrainerController {
+@Autowired
+UserRepository users;
 
 	@RequestMapping(path = "/midi", method = RequestMethod.GET)
 	public String midiPage(Model model, HttpSession session) {
@@ -21,5 +24,16 @@ public class MusicTheoryTrainerController {
 	@RequestMapping(path = "/home", method = RequestMethod.GET)
 	public String home(Model model, HttpSession session) {
 		return "home";
+	}
+
+	@RequestMapping(path = "/register", method = RequestMethod.GET)
+	public String newPlayer(HttpSession session, String firstName, String lastName, String email, String password){
+		User user = users.findByEmail(email);
+		if(user == null){
+			user = new User(firstName,lastName,email,password);
+			users.save(user);
+		}
+		session.setAttribute("user",user);
+		return "redirect:/gamemenu";
 	}
 }
