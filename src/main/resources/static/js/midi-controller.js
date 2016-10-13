20f8258235927b9263c2497e61cbfe2f92396922
Clayton Strickland <cjstrickland5@gmail.com>
2016-10-13 16:36:00 -0400

    Syncing code and about to pair with brice on unit testing

--- src/main/resources/static/js/midi-controller.js
+++ src/main/resources/static/js/midi-controller.js
@@ -75,35 +75,6 @@ angular.module('MidiApp', [])
             this.audioletApp = new AudioletApp();
         };
 
-//        $scope.score = function() {
-
-        // Create an SVG renderer and attach it to the DIV element named "boo".
-//        var vf = new VF.Factory({renderer: {selector: 'boo'}});
-//        var vf2 = new VF.Factory({renderer: {selector: 'boo2'}});
-//        var score = vf.EasyScore();
-//        var score2 = vf2.EasyScore();
-//        var system = vf.System();
-//        var system2 = vf2.System();
-
-
-//        system.addStave({
-//          voices: [score.voice(score.notes('C#5/q, B4, A4, G#4'))]
-//        }).addClef('treble').addTimeSignature('4/4');
-//
-//        vf.draw();
-
-
-//
-//        system2.addStave({
-//          voices: [score2.voice(score2.notes('A4/w'))]
-//        }).addClef('treble');
-//
-//        vf2.draw();
-
-
-
-//        }
-
         $scope.userInput = function(noteContainer) {
             var vf = new VF.Factory({renderer: {selector: 'boo'}});
             var score = vf.EasyScore();
