d929ea422a325ca712c58e2d38d428b3f9f3c1b9
Brett Gordon <gordonbrett89@gmail.com>
2016-10-18 12:48:48 -0400

    text

--- src/main/java/com/tiy/MusicTheoryTrainer/MusicTheoryTrainerController.java
+++ src/main/java/com/tiy/MusicTheoryTrainer/MusicTheoryTrainerController.java
@@ -213,7 +213,7 @@ public class MusicTheoryTrainerController {
 		User myUser = (User) session.getAttribute("user");
 		session.setAttribute("user", myUser);
 
-		return"intervalGameMenu.html";
+		return"IntervalGameMenu.html";
 	}
 
 	@RequestMapping(path = "/intGame", method = RequestMethod.GET)
@@ -221,7 +221,7 @@ public class MusicTheoryTrainerController {
 		User myUser = (User) session.getAttribute("user");
 		session.setAttribute("user", myUser);
 
-		return"intervalGame.html";
+		return"IntervalGame.html";
 	}
 
 //	@RequestMapping(path = "/register", method = RequestMethod.GET)
